4fecbf0314a0d6f7e9d026ce24cb0286e906f8e2
kosovic-creator <drasko.kosovic@icloud.com>
2025-05-08 07:54:52 +0200

    session

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 
-
+'use client';
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        {session.user?.email}
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div>
-      <h1>Todo</h1>
-    </div>
+    <>
+      Not signed in
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
 }
